c4c9d94494a1f318fd0d9108b38515aa98996aba
MaxChang3 <36927158+maxchang3@users.noreply.github.com>
2025-09-15 22:11:10 +0800

    fix(Correct-Next-Button): update version to 0.3.1

--- correct-next-button/correct-next-button.user.js
+++ correct-next-button/correct-next-button.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         矫正 b 站自动连播按钮 - 分P、合集、单视频开关分别独立
 // @namespace    http://maxchang.me
-// @version      0.3.0
+// @version      0.3.1
 // @description  关于我不想要哔哩哔哩自动连播只想在分 P 中跳转但是阿 b 把他们混为一谈这件事。
 // @author       MaxChang3
 // @match        https://www.bilibili.com/video/*
